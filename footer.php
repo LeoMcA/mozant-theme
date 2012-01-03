6d8bd5a4a3f1a115e7ff4a274407653161f14dc3
Leo McArdle <leomcardle@gmail.com>
2012-01-03 14:49:49 +0000

    NO ESCAPE FROM REALITY

--- footer.php
+++ footer.php
@@ -9,7 +9,8 @@
             <div class="container_12">
                 <div class="grid_3 legal_notice">
                     <img src="<?php bloginfo("template_url"); ?>/img/mozilla_community_logo_white.svg">
-                    Except where otherwise noted, content on this site is licensed under the <a href="#">Creative Commons Attribution Share-Alike License v3.0</a> or any later version.
+                    Except where otherwise noted, content on this site is licensed under the <a href="https://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution Share-Alike License v3.0</a> or any later version.<br>
+                    All rights in the names, trademarks, and logos of the Mozilla Foundation are owned exclusively by the Mozilla Foundation.
                 </div>
                 <?php dynamic_sidebar('sidebar-footer'); ?>
             </div>
